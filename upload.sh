20309b69f4a69fba1ed99673e895af454a0fe300
Rachel Schwartz <51383714+rachadele@users.noreply.github.com>
2023-09-01 12:06:40 -0700

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#exit when any command fails
+set -e
+#keep track of last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
+
+read -erp "Enter GEO accession: " GSE
+read -erp "Enter your username: " username
+
+source /hive/users/$username/envs/hca-util-env/bin/activate
+hca-util create $GSE
+echo "enter upload area uuid"
+read uuid
+hca-util select $uuid
+aws s3 cp ./ s3://hca-util-upload-area/$uuid --recursive --exclude "*" --include "*"
+echo "upload complete"
+
+read -erp "Enter submission uuid: " submission
+hca-util sync $submission
